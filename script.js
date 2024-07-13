6e3b4259b6b04ee9d1b6871183a5a8fbe07c3704
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-07-13 16:24:19 +0200

    remove commented lines of codes

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ closeNav.addEventListener("click", () => {
 
 // Features Toggle
 
-const featuresId = document.querySelectorAll('[data-id]')
-const featured = document.querySelectorAll('.feature')
+const featuresId = document.querySelectorAll("[data-id]");
+const featured = document.querySelectorAll(".feature");
 featuresId.forEach((features) => {
-  features.addEventListener('click', () => {
-    const dataId = features.dataset.id
-    const data = document.getElementById(dataId)
-    const prevEl = features.previousElementSibling
+  features.addEventListener("click", () => {
+    const dataId = features.dataset.id;
+    const data = document.getElementById(dataId);
+    const prevEl = features.previousElementSibling;
     const pre = document.querySelector(".active");
     featured.forEach((featur) => {
       const prevFeature = featur.previousElementSibling;
@@ -36,7 +36,7 @@ featuresId.forEach((features) => {
         // prevEl.classList.remove('active')
         data.classList.toggle("active");
       } else {
-        pre.classList.remove('active')
+        pre.classList.remove("active");
         data.classList.toggle("active");
         prevFeature.classList.remove("active");
         features.classList.toggle("active");
@@ -44,37 +44,8 @@ featuresId.forEach((features) => {
       }
     });
     // console.log(data)
-  })
-})
-// console.log(featuresId)
-
-
-// document.addEventListener('click', (e) => {
-//     const feature = document.querySelectorAll('[data-id')
-//     const featureid = e.target.matches("[data-id");
-//     const fId = e.target.closest('[data-id]')
-//     const featureDesc = document.querySelectorAll('.feature')
-//         // console.log(featureDesc)
-//     fId.addEventListener('click', () => {
-//         if (!featureid && e.target.closest("[data-id]") != null)
-//           return;
-//    let currentFeature;
-//    if (featureid) {
-//      currentFeature = e.target.closest("[data-id]");
-//      // console.log(currentDropdown)
-//        currentFeature.classList.toggle("active");
-//     //    featureDesc.forEach(add => {
-//     //        add.classList.toggle('active')
-//     //    })
-//    }
-//    document.querySelectorAll("[data-id].active").forEach((feat) => {
-//      if (feat === currentFeature) return;
-//      feat.classList.remove("active");
-//    });
-//     })
-// })
-
-// FAQ
+  });
+});
 
 const questionImg = document.querySelectorAll(".question");
 const answers = document.querySelectorAll(".answer");
@@ -83,11 +54,12 @@ questionImg.forEach((quest) => {
   quest.addEventListener("click", () => {
     const prevEl = quest.previousElementSibling;
     if (!prevEl) {
-      quest.classList.toggle('active')
-    } else if (!prevEl.classList.contains('active')) {
-      quest.classList.toggle('active')
+      quest.classList.toggle("active");
+    } else if (!prevEl.classList.contains("active")) {
+      quest.classList.toggle("active");
     } else {
-      prevEl.classList.remove('active');
-      quest.classList.toggle('active')
+      prevEl.classList.remove("active");
+      quest.classList.toggle("active");
     }
-})})
+  });
+});
